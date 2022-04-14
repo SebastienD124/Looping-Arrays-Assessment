e2e52e17c2f007400e7c601fbbc1d6156e4d48a2
Sebastien Derilus <sebastienderilus@Sebastiens-MacBook-Pro.local>
2022-04-14 17:26:50 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+for (let i = 1000; i >= 0; i-=2)
+ {
+    console.log(i);
+    
+  }
+
+    for (let i = 0; i <= 10000; i++) {
+       if (i==2500) 
+        alert("A quarter of the way there!");
+
+       if (i==5000)
+        alert("Halfway there!")
+
+       if (i==10000)
+        alert("The loop is done!")
+        }
+
+const tvShows = ["One Piece","Dragon Ball Z","Naruto","Black Clover","Demon Slayer"]
+console.log(tvShows)
+for(let i = 0; i < tvShows.length; i++){
+  console.log("My #" + (i+1) +" favorite TV show is "+ tvShows[i])
+}
+
+// console.log("My #1 favorite TV show is "+ tvShows[0]);
+// console.log("My #2 favorite TV show is "+ tvShows[1]);
+// console.log("My #3 favorite TV show is "+ tvShows[2]);
+// console.log("My #4 favorite TV show is "+ tvShows[3]);
+// console.log("My #5 favorite TV show is "+ tvShows[4]);
